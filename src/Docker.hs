ab4a597e04933e20a6a37e7decf942209e807457
Deni Bertovic <deni@denibertovic.com>
2016-03-03 15:19:03 +0100

    More fleshing out
    Adds all the functions we have in master but whithout
    implementation.

--- src/Docker.hs
+++ src/Docker.hs
@@ -10,3 +10,31 @@ listContainers = undefined
 
 listImages :: Monad m => ListOpts -> DockerT m ([Image])
 listImages = undefined
+
+createContainer :: Monad m => CreateOpts -> DockerT m (ContainerID)
+createContainer  = undefined
+
+startContainer :: Monad m => StartOpts -> DockerT m (ContainerID)
+startContainer = undefined
+
+stopContainer :: Monad m => Timeout -> DockerT m ()
+stopContainer = undefined
+
+killContainer :: Monad m => Signal -> DockerT m ()
+killContainer = undefined
+
+restartContainer :: Monad m => Timeout -> DockerT m ()
+restartContainer = undefined
+
+pauseContainer :: Monad m => DockerT m ()
+pauseContainer = undefined
+
+unpauseContainer :: Monad m => DockerT m ()
+unpauseContainer = undefined
+
+deleteContainer :: Monad m => DeleteOpts -> DockerT m ()
+deleteContainer = undefined
+
+getContainerLogs :: Monad m => LogOpts -> DockerT m ()
+getContainerLogs = undefined
+
--- src/Docker/Types.hs
+++ src/Docker/Types.hs
@@ -2,8 +2,9 @@ module Docker.Types where
 
 import           Control.Monad.Except (ExceptT, runExceptT)
 import           Control.Monad.Reader (ReaderT, runReaderT)
-import           Data.Aeson           (FromJSON, ToJSON, genericParseJSON,
-                                       genericToJSON, parseJSON, toJSON)
+import           Data.Aeson           (FromJSON, ToJSON, encode,
+                                       genericParseJSON, genericToJSON,
+                                       parseJSON, toJSON)
 import           Data.Aeson.Types     (defaultOptions, fieldLabelModifier)
 import qualified Data.ByteString.Lazy as BL
 import           Data.Char            (toUpper)
@@ -15,9 +16,14 @@ import qualified Network.HTTP.Client  as HTTP
 type Endpoint = Text
 type URL = Text
 type ApiVersion = Text
+type ContainerID = Text
+type ImageID = Text
+type Timeout = Integer
 
 type Request = HTTP.Request
 type Response = HTTP.Response BL.ByteString
+
+data Signal = SIGINT | SIGKILL deriving (Eq, Show)
 
 data DockerClientOpts m = DockerClientOpts {
       http       :: Request -> m Response
@@ -31,22 +37,17 @@ runDockerT :: (Monad m) => DockerClientOpts m -> DockerT m a -> m (Either String
 runDockerT opts a = runExceptT $ runReaderT a opts
 -- runDockerT opts a = (runExceptT .) . flip runReaderT opts a
 
-data ListOpts = ListOpts
-              { all :: Bool
-              } deriving (Eq, Show, Generic)
-
-instance ToJSON ListOpts
-instance FromJSON ListOpts
+data ListOpts = ListOpts { all :: Bool } deriving (Eq, Show)
 
 defaultClientOpts :: DockerClientOpts IO
-defaultClientOpts = DockerClientOpts
-                { http = undefined
+defaultClientOpts = DockerClientOpts {
+                  http = undefined
                 , apiVersion = "v1.22"
                 , baseUrl = "http://127.0.0.1:2375/"
                 }
 
-data DockerVersion = DockerVersion
-                  { version       :: Text
+data DockerVersion = DockerVersion {
+                    version       :: Text
                   , gitCommit     :: Text
                   , goVersion     :: Text
                   , arch          :: Text
@@ -67,4 +68,22 @@ data Container = Container
 
 data Image = Image
 
+data CreateOpts = CreateOpts
+data StartOpts = StartOpts
 
+data DeleteOpts = DeleteOpts {
+                  v     :: Bool
+                , force :: Bool
+                } deriving (Eq, Show)
+
+type Timestamp = Integer
+data Tail = Integer | All deriving (Eq, Show)
+
+data LogOpts = LogOpts {
+               follow     :: Bool
+             , stdout     :: Bool
+             , stderr     :: Bool
+             , since      :: Timestamp
+             , timestamps :: Bool
+             , tail       :: Tail
+             } deriving (Eq, Show)
