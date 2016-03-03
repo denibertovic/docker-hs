4e85f3f4eba0b14f5a40e5a56861296ea00aea5b
Deni Bertovic <deni@denibertovic.com>
2016-03-03 12:31:56 +0100

    Adds basic layout
    Caries errror and reader and add runDockerT.

--- src/Docker.hs
+++ src/Docker.hs
@@ -1,2 +1,12 @@
 module Docker where
 
+import           Docker.Types
+
+getDockerVersion :: Monad m => DockerT m (DockerVersion)
+getDockerVersion = undefined
+
+listContainers :: Monad m => ListOpts -> DockerT m ([Container])
+listContainers = undefined
+
+listImages :: Monad m => ListOpts -> DockerT m ([Image])
+listImages = undefined
--- src/Docker/Types.hs
+++ src/Docker/Types.hs
@@ -1,23 +1,47 @@
 module Docker.Types where
 
-import           Data.Aeson       (FromJSON, ToJSON, genericParseJSON,
-                                   genericToJSON, parseJSON, toJSON)
-import           Data.Aeson.Types (defaultOptions, fieldLabelModifier)
-import           Data.Char        (toUpper)
-import           Data.Text        (Text)
-import           GHC.Generics     (Generic)
+import           Control.Monad.Except (ExceptT, runExceptT)
+import           Control.Monad.Reader (ReaderT, runReaderT)
+import           Data.Aeson           (FromJSON, ToJSON, genericParseJSON,
+                                       genericToJSON, parseJSON, toJSON)
+import           Data.Aeson.Types     (defaultOptions, fieldLabelModifier)
+import qualified Data.ByteString.Lazy as BL
+import           Data.Char            (toUpper)
+import           Data.Text            (Text)
+import qualified Data.Text            as T
+import           GHC.Generics         (Generic)
+import qualified Network.HTTP.Client  as HTTP
 
+type Endpoint = Text
 type URL = Text
 type ApiVersion = Text
 
-data DockerClientOpts = DockerClientOpts {
-      apiVersion :: ApiVersion
-    , baseUrl    :: URL
-    } deriving (Show)
+type Request = HTTP.Request
+type Response = HTTP.Response BL.ByteString
 
-defaultClientOpts :: DockerClientOpts
+data DockerClientOpts m = DockerClientOpts {
+      http       :: Request -> m Response
+    , apiVersion :: ApiVersion
+    , baseUrl    :: URL
+    }
+
+type DockerT m a = ReaderT (DockerClientOpts m) (ExceptT String m) a
+
+runDockerT :: (Monad m) => DockerClientOpts m -> DockerT m a -> m (Either String a)
+runDockerT opts a = runExceptT $ runReaderT a opts
+-- runDockerT opts a = (runExceptT .) . flip runReaderT opts a
+
+data ListOpts = ListOpts
+              { all :: Bool
+              } deriving (Eq, Show, Generic)
+
+instance ToJSON ListOpts
+instance FromJSON ListOpts
+
+defaultClientOpts :: DockerClientOpts IO
 defaultClientOpts = DockerClientOpts
-                { apiVersion = "v1.22"
+                { http = undefined
+                , apiVersion = "v1.22"
                 , baseUrl = "http://127.0.0.1:2375/"
                 }
 
@@ -31,10 +55,16 @@ data DockerVersion = DockerVersion
 
 
 instance ToJSON DockerVersion where
-  toJSON = genericToJSON defaultOptions {
-             fieldLabelModifier = (\(x:xs) -> toUpper x : xs)}
+    toJSON = genericToJSON defaultOptions {
+         fieldLabelModifier = (\(x:xs) -> toUpper x : xs)}
 
 instance FromJSON DockerVersion where
-  parseJSON = genericParseJSON defaultOptions {
-                fieldLabelModifier = (\(x:xs) -> toUpper x : xs)}
+    parseJSON = genericParseJSON defaultOptions {
+            fieldLabelModifier = (\(x:xs) -> toUpper x : xs)}
+
+
+data Container = Container
+
+data Image = Image
+
 
