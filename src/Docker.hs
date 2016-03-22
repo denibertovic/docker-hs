615cbd1a828a6576b00b4b491e1fa23aa0927ae9
Deni Bertovic <deni@denibertovic.com>
2016-03-22 17:47:42 +0100

    Remove unused type variable

--- src/Docker.hs
+++ src/Docker.hs
@@ -16,7 +16,7 @@ import           Network.HTTP.Types     (StdMethod (..))
 import           Docker.Internal
 import           Docker.Types
 
-getDockerVersion :: forall m a. MonadThrow m => DockerT m DockerVersion
+getDockerVersion :: forall m. MonadThrow m => DockerT m DockerVersion
 getDockerVersion = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest GET VersionEndpoint opts
@@ -25,7 +25,7 @@ getDockerVersion = do
     let res = decode body
     return $ fromJust res
 
-listContainers :: forall m a. MonadThrow m => ListOpts -> DockerT m [Container]
+listContainers :: forall m. MonadThrow m => ListOpts -> DockerT m [Container]
 listContainers lopts = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest GET (ListContainersEndpoint lopts) opts
@@ -34,7 +34,7 @@ listContainers lopts = do
     let res = decode body
     return $ fromJust res
 
-listImages :: forall m a. MonadThrow m => ListOpts -> DockerT m [Image]
+listImages :: forall m. MonadThrow m => ListOpts -> DockerT m [Image]
 listImages lopts = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest GET (ListImagesEndpoint lopts) opts
@@ -43,58 +43,59 @@ listImages lopts = do
     let res = decode body
     return $ fromJust res
 
-createContainer :: forall m a. MonadThrow m => CreateOpts -> DockerT m ContainerID
+createContainer :: forall m. MonadThrow m => CreateOpts -> DockerT m ContainerID
 createContainer  = undefined
 
-startContainer :: forall m a. MonadThrow m => StartOpts -> ContainerID -> DockerT m ContainerID
+startContainer :: forall m. MonadThrow m => StartOpts -> ContainerID -> DockerT m ContainerID
 startContainer sopts cid = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest POST (StartContainerEndpoint sopts cid) opts
     _ <- lift $ ((httpHandler request) :: m Response)
     return cid
 
-stopContainer :: forall m a. MonadThrow m => Timeout -> ContainerID ->DockerT m ContainerID
+stopContainer :: forall m. MonadThrow m => Timeout -> ContainerID ->DockerT m ContainerID
 stopContainer t cid = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest POST (StopContainerEndpoint t cid) opts
     _ <- lift $ ((httpHandler request) :: m Response)
     return cid
 
-killContainer :: forall m a. MonadThrow m => Signal -> ContainerID -> DockerT m ContainerID
+killContainer :: forall m. MonadThrow m => Signal -> ContainerID -> DockerT m ContainerID
 killContainer s cid = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest POST (KillContainerEndpoint s cid) opts
     _ <- lift $ ((httpHandler request) :: m Response)
     return cid
 
-restartContainer :: forall m a. MonadThrow m => Timeout -> ContainerID -> DockerT m ContainerID
+restartContainer :: forall m. MonadThrow m => Timeout -> ContainerID -> DockerT m ContainerID
 restartContainer t cid = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest POST (RestartContainerEndpoint t cid) opts
     _ <- lift $ ((httpHandler request) :: m Response)
     return cid
 
-pauseContainer :: forall m a. MonadThrow m => ContainerID -> DockerT m ContainerID
+pauseContainer :: forall m. MonadThrow m => ContainerID -> DockerT m ContainerID
 pauseContainer cid = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest POST (PauseContainerEndpoint cid) opts
     _ <- lift $ ((httpHandler request) :: m Response)
     return cid
 
-unpauseContainer :: forall m a. MonadThrow m => ContainerID -> DockerT m ContainerID
+unpauseContainer :: forall m. MonadThrow m => ContainerID -> DockerT m ContainerID
 unpauseContainer cid = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest GET (UnpauseContainerEndpoint cid) opts
     _ <- lift $ ((httpHandler request) :: m Response)
     return cid
 
-deleteContainer :: forall m a. MonadThrow m => DeleteOpts -> ContainerID -> DockerT m ContainerID
+deleteContainer :: forall m. MonadThrow m => DeleteOpts -> ContainerID -> DockerT m ContainerID
 deleteContainer dopts cid = do
     (opts, httpHandler) <- ask
     let request = fromJust $ mkHttpRequest DELETE (DeleteContainerEndpoint dopts cid) opts
     _ <- lift $ ((httpHandler request) :: m Response)
     return cid
 
-getContainerLogs ::  forall m a. MonadThrow m => LogOpts -> ContainerID -> DockerT m ()
-getContainerLogs = undefined
+getContainerLogs ::  forall m. MonadThrow m => LogOpts -> ContainerID -> DockerT m ()
+getContainerLogs logopts cid = undefined
+
 
