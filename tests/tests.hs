ae66f6951ecd90fc5c60a324b3efd12b7112810b
James Parker <dev@jamesparker.me>
2016-08-02 12:30:51 -0400

    include inspect container in tests

--- tests/tests.hs
+++ tests/tests.hs
@@ -56,6 +56,7 @@ runAndReadLog = runDocker $ do
     containerId <- createContainer (defaultCreateOpts (testImageName <> ":latest"))
     c <- fromRight containerId
     status1 <- startContainer defaultStartOpts c
+    _ <- inspectContainer c >>= fromRight
     lift $ threadDelay 300000 -- give 300ms for the application to finish
     lift $ assert $ status1 == Right ()
     status2 <- killContainer SIGTERM c
