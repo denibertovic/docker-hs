4d1ba7d6e1b817db30d54d1df996c4e127231b9a
Tom McLaughlin <pyro777@gmail.com>
2018-08-12 19:00:16 -0700

    Use hspec for integration tests we can use beforeAll hook

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -6,19 +6,21 @@ import Data.Maybe
 import System.Environment
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.Hspec
 
 import qualified Tests.Integration as Integration
 import qualified Tests.JSON as JSON
 
 main :: IO ()
 main = do
-  -- useUnixSocket <- lookupEnv "DOCKER_UNIX_SOCKET"
-  let useUnixSocket = Just True
+  useUnixSocket <- lookupEnv "DOCKER_UNIX_SOCKET"
   let runDocker = if isJust useUnixSocket then Integration.runDockerUnix else Integration.runDockerHTTP
 
   maybeRunIntegrationTests <- lookupEnv "RUN_INTEGRATION_TESTS"
-  let tests = case maybeRunIntegrationTests of
-        Nothing -> [JSON.tests]
-        Just _ -> [JSON.tests, Integration.tests runDocker]
+  tests <- case maybeRunIntegrationTests of
+             Nothing -> return [JSON.tests]
+             Just _ -> do
+               integrationTests <- testSpecs $ Integration.tests runDocker
+               return $ JSON.tests : integrationTests
 
   defaultMain $ testGroup "Tests" tests
--- tests/Tests/Integration.hs
+++ tests/Tests/Integration.hs
@@ -2,14 +2,6 @@
 
 module Tests.Integration (tests, runDockerHTTP, runDockerUnix) where
 
-import           Data.Either               (rights)
-import           Data.Maybe
-import           Prelude                   hiding (all)
-import qualified Test.QuickCheck.Monadic   as QCM
-import           Test.Tasty
-import           Test.Tasty.HUnit
-import           Test.Tasty.QuickCheck     (testProperty)
-
 import           Control.Concurrent        (threadDelay)
 import           Control.Lens              ((^.), (^?))
 import           Control.Monad             (forM_)
@@ -17,27 +9,31 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class (lift)
 import           Data.Aeson                ((.=))
 import qualified Data.Aeson                as JSON
-import           Data.Aeson.Lens           (key, _Array, _Null, _Object,
-                                            _String, _Value)
+import           Data.Aeson.Lens           (key, _Array, _Null, _Object, _String, _Value)
 import qualified Data.ByteString           as B
 import qualified Data.ByteString.Char8     as C
 import qualified Data.ByteString.Lazy      as BL
+import           Data.Either               (rights)
 import qualified Data.HashMap.Strict       as HM
-import           Data.Int                  (Int)
 import qualified Data.Map                  as M
+import           Data.Maybe
 import           Data.Maybe                (fromJust, isJust, isNothing, listToMaybe)
 import           Data.Monoid
 import           Data.Text                 (Text, unpack)
 import qualified Data.Vector               as V
+import           Docker.Client
 import           Network.Connection        (TLSSettings (..))
 import           Network.HTTP.Client       (newManager)
 import           Network.HTTP.Client.TLS
 import           Network.HTTP.Types.Status
+import           Prelude                   hiding (all)
 import           System.Directory          (getCurrentDirectory)
 import           System.Environment        (lookupEnv)
 import           System.Process            (system)
-
-import           Docker.Client
+import           Test.Hspec
+import qualified Test.QuickCheck.Monadic   as QCM
+import           Test.Tasty
+import           Test.Tasty.Hspec
 
 -- opts = defaultClientOpts
 testImageName = "docker-hs/test"
@@ -65,18 +61,18 @@ runDockerUnix cmd = do
 testDockerVersion :: RunDockerCmd -> IO ()
 testDockerVersion runDocker = runDocker $ do
   v <- getDockerVersion
-  lift $ assert $ isRight v
+  lift $ isRight v `shouldBe` True
 
 testStopNonexisting :: RunDockerCmd -> IO ()
 testStopNonexisting runDocker = runDocker $ do
   res <- stopContainer DefaultTimeout $ fromJust $ toContainerID "thisshouldnotexist"
-  lift $ assert $ isLeft res
+  lift $ isLeft res `shouldBe` True
 
 testFindImage :: RunDockerCmd -> IO ()
 testFindImage runDocker = runDocker $ do
   images <- listImages defaultListOpts >>= fromRight
   let xs = filter (elem imageFullName . imageRepoTags) images
-  lift $ assert $ length xs == 1
+  lift $ length xs `shouldBe` 1
 
   where imageFullName = testImageName <> ":latest"
 
@@ -86,9 +82,9 @@ testDeleteImage runDocker = runDocker $ do
   let img = fromMaybe (error "Couldn't find test image") maybeTestImage
 
   result <- deleteImage defaultImageDeleteOpts (imageId img)
-  lift $ assert $ isRight result
+  lift $ isRight result `shouldBe` True
   maybeTestImageAfter <- findTestImage
-  lift $ assert $ isNothing maybeTestImageAfter
+  lift $ maybeTestImageAfter `shouldBe` Nothing
   where
     findTestImage = do
       images <- listImages defaultListOpts >>= fromRight
@@ -101,7 +97,7 @@ testListContainers runDocker = runDocker $ do
   c <- fromRight containerId
   res <- listContainers $ ListOpts { all=True }
   deleteContainer (ContainerDeleteOpts True True) c
-  lift $ assert $ isRight res
+  lift $ isRight res `shouldBe` True
 
 testBuildFromDockerfile :: RunDockerCmd -> IO ()
 testBuildFromDockerfile runDocker = do
@@ -109,8 +105,7 @@ testBuildFromDockerfile runDocker = do
   let ctxDir = cur ++ "/tests"
   runDocker $ do
     r <- buildImageFromDockerfile (defaultBuildOpts "docker-hs/dockerfile-test") ctxDir
-    lift $ assert $ isRight r
-
+    lift $ isRight r `shouldBe` True
 
 testRunAndReadLog :: RunDockerCmd -> IO ()
 testRunAndReadLog runDocker = testRunAndReadLogHelper runDocker $ NetworkingConfig HM.empty
@@ -129,7 +124,7 @@ testRunAndReadLogHelper runDocker networkingConfig = runDocker $ do
   lift $ threadDelay 300000 -- give 300ms for the application to finish
   lift $ assertBool ("starting the container, unexpected status: " ++ show status1) $ isRightUnit status1
   logs <- getContainerLogs defaultLogOpts c >>= fromRight
-  lift $ assert $ (C.pack "123") `C.isInfixOf` (toStrict1 logs)
+  lift $ ((C.pack "123") `C.isInfixOf` (toStrict1 logs)) `shouldBe` True
   status3 <- deleteContainer (ContainerDeleteOpts True True) c
   lift $ assertBool ("deleting container, unexpected status: " ++ show status3) $ isRightUnit status3
   mapM_ removeNetwork createdNetworks
@@ -153,22 +148,23 @@ testCreateInspectRemoveNetwork runDocker = runDocker $ do
   removeStatus <- removeNetwork nid
   lift $ assertBool ("removing a network, unexpected status: " ++ show removeStatus) $ isRight removeStatus
 
-tests :: RunDockerCmd -> TestTree
-tests runDocker = testGroup "Integration Tests" [
-  testCase "Get docker version" (testDockerVersion runDocker)
-  , testCase "Build image from Dockerfile" (testBuildFromDockerfile runDocker)
-  , testCase "Find image by name" (testFindImage runDocker)
-  , testCase "Delete image" (testDeleteImage runDocker)
-  , testCase "List containers" (testListContainers runDocker)
-  , testCase "Run a dummy container and read its log" (testRunAndReadLog runDocker)
-  , testCase "Run a dummy container with networking and read its log" (testRunAndReadLogWithNetworking runDocker)
-  , testCase "Try to stop a container that doesn't exist" (testStopNonexisting runDocker)
-  , testCase "Create, inspect, and remove a network" (testCreateInspectRemoveNetwork runDocker)
-  ]
+tests runDocker = describe "Integration tests" $ beforeAll_ setup $ do
+  it "Get docker version" $ testDockerVersion runDocker
+  it "Build image from Dockerfile" $ testBuildFromDockerfile runDocker
+  it "Find image by name" $ testFindImage runDocker
+  it "Delete image" $ testDeleteImage runDocker
+  it "List containers" $ testListContainers runDocker
+  it "Run a dummy container and read its log" $ testRunAndReadLog runDocker
+  it "Run a dummy container with networking and read its log" $ testRunAndReadLogWithNetworking runDocker
+  it "Try to stop a container that doesn't exist" $ testStopNonexisting runDocker
+  it "Create, inspect, and remove a network" $ testCreateInspectRemoveNetwork runDocker
 
 setup :: IO ()
 setup = mapM_ system [ "docker pull " ++ unpack imageToDeleteFullName
                      , "docker build -t " ++ unpack testImageName ++ " tests"]
+
+assertBool message True = return ()
+assertBool message False = error message
 
 isLeft :: Either a b -> Bool
 isLeft = not . isRight
@@ -178,13 +174,11 @@ isRight (Left _)  = False
 isRight (Right _) = True
 
 fromRight :: (MonadIO m, Show l) => Either l r -> m r
-fromRight (Left l) = do
-  liftIO $ assertFailure $ "Left: " ++ show l
-  undefined
+fromRight (Left l) = liftIO $ error $ "Left: " ++ show l
 fromRight (Right r) = return r
 
 main :: IO ()
 main = do
   useUnixSocket <- lookupEnv "DOCKER_UNIX_SOCKET"
   let runDocker = if isJust useUnixSocket then runDockerUnix else runDockerHTTP
-  defaultMain $ tests runDocker
+  hspec $ tests runDocker
